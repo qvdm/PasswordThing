25907600ea363f409689670c3984d65e9ac98e03
quentin <q@uent.in>
2020-02-07 22:34:24 -0800

    ver

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "82"
+  #define BUILD_NUMBER "87"
 #endif
 #ifndef VERSION
-  #define VERSION "V5.1.82 - 2020-02-03 12:29:00.712000"
+  #define VERSION "V5.1.87 - 2020-02-05 07:43:31.843000"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "V5.1.82"
+  #define VERSION_SHORT "V5.1.87"
 #endif
