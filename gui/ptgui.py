26f8c60439eef910c0291011719fa0aaa0534ff3
Quentin <q@uent.in>
2020-02-07 16:50:45 -0800

    gui top line

--- gui/ptgui.py
+++ gui/ptgui.py
@@ -1,11 +1,17 @@
 # menu.py
 import sys
 import glob
-import tkinter as tk
-from tkinter import messagebox
+try:
+    import tkinter as tk
+    from tkinter import messagebox
+except:
+    import Tkinter as tk
+    import tkMessageBox as messagebox
 import pygubu
 import serial
 
+
+__VERSION__ = '0.1'
 
 def serial_ports():
     """ Lists serial port names
@@ -36,23 +42,33 @@ def serial_ports():
     return result
 
 
-class MyApplication(pygubu.TkApplication):
+class Application(pygubu.TkApplication):
 
     def _create_ui(self):
-        #1: Create a builder
+        self.dirty=False
+        self.port=None
+        self.baud=115200
+        self.serial=None
+
         self.builder = builder = pygubu.Builder()
 
-        #2: Load an ui file
         builder.add_from_file('menu.ui')
 
-        #3: Create the widget using self.master as parent
         self.mainwindow = builder.get_object('mainwindow', self.master)
-
-        # Set main menu
         self.mainmenu = menu = builder.get_object('mainmenu', self.master)
         self.set_menu(menu)
 
-        # Configure callbacks
+        self.mainlabel = mainlabel = builder.get_object('lmain', self.master)
+        self.builder.tkvariables['strlmain'].set("PwThing Version " + __VERSION__)
+
+        self.portcombo = portcombo = builder.get_object('cport', self.master)
+        portcombo['values'] = portlist
+        portcombo.current(0)
+    
+        self.master.protocol("WM_DELETE_WINDOW", self.on_close_window)
+
+        self.termbox = termbox = builder.get_object('tterm', self.master)
+
         builder.connect_callbacks(self)
 
 
@@ -61,8 +77,7 @@ class MyApplication(pygubu.TkApplication):
             messagebox.showinfo('File', 'You clicked Open menuitem')
 
         if itemid == 'mfile_quit':
-            messagebox.showinfo('File', 'You clicked Quit menuitem. Byby')
-            self.quit();
+            self.on_close_window();
 
     def on_mtools_item_clicked(self, itemid):
         if itemid == 'mtools_dump':
@@ -73,11 +88,57 @@ class MyApplication(pygubu.TkApplication):
     def on_about_clicked(self):
         messagebox.showinfo('About', 'You clicked About menuitem')
 
+    def on_port_selected(self, event):
+        self.port = self.builder.tkvariables.__getitem__('tvport').get() 
+
+    def on_rescan_ports(self):
+        portlist = serial_ports()
+        portlist.insert(0,'')
+        self.portcombo['values'] = portlist
+        self.portcombo.current(0)
+
+    def on_connect(self):
+        if self.port != None:
+            if self.serial == None:
+                self.serial = serial.Serial(port=self.port, baudrate=self.baud)
+            else:
+                self.serial.close()
+                self.serial = serial.Serial(port=self.port, baudrate=self.baud)
+            self.termbox.insert(tk.INSERT, "===========Port %s opened>>>\n"%self.port,"info")
+            self.Serial_term()
+        else:
+            messagebox.showerror('Error', 'No port selected')
+           
+
+    def on_close_window(self, event=None):
+        if self.dirty:
+            msg = messagebox.askquestion ('Exit Configurator','There are unsaved changes.  Are you sure you want to exit',icon = 'warning')
+            if msg != 'yes':
+                return
+
+        if self.serial != None:
+            self.serial.close()
+        
+        # Call destroy on toplevel to finish program
+        self.mainwindow.master.destroy()
+
+    def Serial_term(self):
+        if self.serial != None:
+            rx = []
+            while (self.serial.inWaiting()>0):
+                rx.append(ord(self.serial.read(1)))
+                time.sleep(0.001)
+            if rx != []:
+                for s in rx:
+                    self.termbox.insert(tk.INSERT, "%c"%s)
+                if(rx[-1] != 0x0d): self.termbox.insert(tk.INSERT, "\n")
+                if(self.autoscroll): self.termbox.see("end")
 
 if __name__ == '__main__':
-    ports = serial_ports()
+    portlist = serial_ports()
+    portlist.insert(0,'')
     root = tk.Tk()
-    app = MyApplication(root)
+    app = Application(root)
     app.run()
     
     
