3f471e4fd287c2c32dede61bdb6ca8d64c58e0d7
Quentin <q@uent.in>
2020-03-02 14:49:27 -0800

    sf

--- gui/uart.py
+++ gui/uart.py
@@ -4,46 +4,64 @@ import time
 import queue
 import tkinter as tk
 
-
 class SerialThread(threading.Thread):
-    def __init__(self, queue):
+    def __init__(self, rxqueue, txqueue):
         threading.Thread.__init__(self)
-        self.queue = queue
+        self.rxqueue = rxqueue
+        self.txqueue = txqueue
     def run(self):
-        s = serial.Serial('COM4',115200)
-        s.write(str.encode('*00T%'))
+        s = serial.Serial('COM8',115200)
+        s.write(str.encode('\n'))
         time.sleep(0.2)
         while True:
             if s.inWaiting():
                 text = s.readline(s.inWaiting())
-                self.queue.put(text)
+                self.rxqueue.put(text)
+            if self.txqueue.qsize():
+                text = self.txqueue.get()
+                s.write(str.encode(text))
 
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.geometry("1360x750")
+        self.lc=0
         frameLabel = tk.Frame(self, padx=40, pady =40)
         self.text = tk.Text(frameLabel, wrap='word', font='TimesNewRoman 37',
                             bg=self.cget('bg'), relief='flat')
         frameLabel.pack()
         self.text.pack()
-        self.queue = queue.Queue()
-        thread = SerialThread(self.queue)
+        self.rxqueue = queue.Queue()
+        self.txqueue = queue.Queue()
+        thread = SerialThread(self.rxqueue, self.txqueue)
         thread.start()
         self.process_serial()
+    
+
 
     def process_serial(self):
         value=True
-        while self.queue.qsize():
+        while self.rxqueue.qsize():
             try:
-                new=self.queue.get()
+                new=self.rxqueue.get()
                 if value:
                     self.text.delete(1.0, 'end')
                 value=False
                 self.text.insert('end',new)
-            except queue.Empty:
+            except self.rxqueue.Empty:
                 pass
+
+        if (self.lc == 0) :
+            pass
+        elif (self.lc == 1) :
+           self.txqueue.put('3123\n')
+        elif (self.lc == 2) :
+            pass
+        elif (self.lc == 3) :
+            self.txqueue.put('0P\n')
+        self.lc = self.lc+1
+
         self.after(100, self.process_serial)
 
 app = App()
-app.mainloop()
+tk.mainloop()
--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "103"
+  #define BUILD_NUMBER "105"
 #endif
 #ifndef VERSION
-  #define VERSION "V5.1.103 - 2020-02-10 19:44:49.269000"
+  #define VERSION "V5.1.105 - 2020-02-10 19:44:49.269000"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "V5.1.103"
+  #define VERSION_SHORT "V5.1.105"
 #endif
